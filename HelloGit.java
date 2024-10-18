aa055776454b79823035acae352838fdeb9eab58
Dinesh Kumar <dinesh2753@outlook.com>
2024-10-18 22:39:43 +0530

    Updated Hello.java

--- HelloGit.java
+++ HelloGit.java
@@ -1,6 +1,6 @@
 public class HelloGit{
     public static void main(String[] args){
         System.out.print("Hello GIT!");
-        System.out.print("I am learning you");
+        System.out.print("I am learning Github");
     }
 }
